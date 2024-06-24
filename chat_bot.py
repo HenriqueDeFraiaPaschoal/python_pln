1a736b49a60af8da991daeb848791fdc0801eafc
Henrique Paschoal <110928555+HenriqueDeFraiaPaschoal@users.noreply.github.com>
2024-06-24 20:10:42 -0300

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -26,9 +26,6 @@ def encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia=5):
 if __name__ == "__main__":
   perguntas_respostas = carregar_perguntas("perguntas.txt")
   limiar_distancia = 10
-  while True:
-    pergunta = "O que e um chatbot?"
-    if pergunta == "sair":
-      break
-    resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
-    print("Resposta:", resposta)
+  pergunta = "O que e um chatbot?"
+  resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
+  print("Resposta:", resposta)
