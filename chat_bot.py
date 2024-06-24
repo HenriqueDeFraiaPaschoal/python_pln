f266eacbddd491a039245b416463e26a46e7b6c6
Henrique Paschoal <110928555+HenriqueDeFraiaPaschoal@users.noreply.github.com>
2024-06-24 20:09:09 -0300

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -25,9 +25,9 @@ def encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia=5):
 
 if __name__ == "__main__":
   perguntas_respostas = carregar_perguntas("perguntas.txt")
-  limiar_distancia = int(input("Digite o limiar de distância para considerar uma pergunta semelhante:"))
+  limiar_distancia = 10
   while True:
-    pergunta = input("Faça uma pergunta:").lower()
+    pergunta = "O que e um chatbot?"
     if pergunta == "sair":
       break
     resposta = encontrar_resposta(pergunta, perguntas_respostas, limiar_distancia)
